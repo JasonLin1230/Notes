5902f58aba6c4087385aac2a32ed7deed96d0411
JasonLin1230 <JasonLin1230@163.com>
2019-06-17 16:59:37 +0800

    UI:ballot_box_with_check:

--- vue-todo/src/app.vue
+++ vue-todo/src/app.vue
@@ -1,18 +1,44 @@
 <template>
-  <div>{{text}}</div>
+  <div id="app">
+    <div id="cover"></div>
+    <Header></Header>
+    <Todo></Todo>
+    <Footer></Footer>
+  </div>
 </template>
 <script>
+import Header from "./todo/header.vue";
+import Todo from "./todo/todo.vue";
+import Footer from "./todo/footer.jsx";
 export default {
   data() {
-    return {
-      text: "abcd"
-    };
+    return {};
+  },
+  components: {
+    Header,
+    Todo,
+    Footer
   }
 };
 </script>
 
-<style scoped>
-div {
-  color: red;
+<style lang="stylus" scoped>
+#app {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+
+  #cover {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: #999;
+    opacity: 0.2;
+    z-index: -1;
+  }
 }
 </style>
--- vue-todo/src/index.js
+++ vue-todo/src/index.js
@@ -1,12 +1,11 @@
-import Vue from 'vue'
-import App from './app.vue'
-import './assets/styles/test.css'
-import './assets/styles/test.styl'
-import './assets/images/bg.png'
+import Vue from "vue";
+import App from "./app.vue";
+import "./assets/styles/global.styl";
+import "./assets/images/bg.png";
 
-const root = document.createElement("div")
-document.body.appendChild(root)
+const root = document.createElement("div");
+document.body.appendChild(root);
 
 new Vue({
-    render: (h) =>h(App)
-}).$mount(root)
+  render: h => h(App)
+}).$mount(root);
