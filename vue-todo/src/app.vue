08232722e19a5238c7204fd93d3bfdd7eed5e0b2
JasonLin1230 <JasonLin1230@163.com>
2019-06-17 10:22:30 +0800

    version rollback

---
+++ vue-todo/src/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>{{text}}</div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            text: "abc"
+        }
+    }
+}
+</script>
+
+<style scoped>
+div{
+    color: red;
+}
+</style>
